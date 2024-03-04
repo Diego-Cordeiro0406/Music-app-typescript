635e146260517dbd942d08e0abad8e5dd73e3a75
Diego Cordeiro <diegocordeiro121@gmail.com>
2024-03-04 18:05:56 -0300

    feat: adicionando links de navegação ao header + testes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,21 @@
 import { readUser } from "../services/userStorage";
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header data-testid="header-component">
       <h1>Header</h1>
+      <div>
+        <Link data-testid="link-to-search" to={"/search"}>
+          <p>Procurar</p>
+        </Link>
+        <Link data-testid="link-to-favorites" to={"/favorites"}>
+          <p>Favoritos</p>
+        </Link>
+        <Link data-testid="link-to-profile" to={"/profile"}>
+          <p>Perfil</p>
+        </Link>
+      </div>
       <div>
         <p data-testid="header-user-name">{readUser().name}</p>
       </div>
