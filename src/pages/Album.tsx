765a6be7f6ca945c0bd29748b55b8388de6ad0b1
Diego Cordeiro <diegocordeiro121@gmail.com>
2024-03-07 17:19:49 -0300

    feat: implementando listagem de musicas por album + testes

--- src/pages/Album.tsx
+++ src/pages/Album.tsx
@@ -1,11 +1,53 @@
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
+import { useEffect, useState } from "react";
+import getMusics from "../services/musicsFetch";
+import { AlbumData, MusicTrack } from "../types/types";
 
 function Album() {
+  const { id } = useParams();
+  const [musicData, setMusicData] = useState<MusicTrack[]>();
+  const [artistData, setArtistData] = useState<AlbumData>();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const data = await getMusics(id!);
+        setMusicData(data);
+        setArtistData(data[0]);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false)
+      }
+    };
+    fetchData();
+  }, [id])
   return (
     <>
     <Header />
     <section data-testid="page-album">
-      <h1>Album</h1>
+      <section>
+        <img src={artistData?.artworkUrl100} alt={artistData?.artistName} />
+        <h3 data-testid="album-name">{ artistData?.collectionName }</h3>
+        <p data-testid="artist-name">{ artistData?.artistName }</p>
+      </section>
+      <main>
+        {
+          loading ? <div data-testid="loading-element">Loading...</div> : (
+            musicData?.slice(1).map((music) => (
+            <div key={music.trackId}>
+              <h3>{ music.trackName }</h3>
+              <audio data-testid="audio-component" src={music.previewUrl} controls>
+                <track kind="captions" />
+                O seu navegador não suporta o elemento{" "} <code>audio</code>.
+              </audio>
+            </div>
+          )))
+        }
+      </main>
     </section>
     </>
   )
